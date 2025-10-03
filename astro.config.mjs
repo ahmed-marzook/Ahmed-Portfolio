66e54ebf0c359ab49d6e214bd2b260f8e3a22ef3
Ahmed Marzook <43218910+ahmed-marzook@users.noreply.github.com>
2025-10-03 22:57:34 +0100

    Configure Astro base for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import react from '@astrojs/react';
-
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ahmed-marzook.github.io',
+  base: '/Ahmed-Portfolio',
   integrations: [react()],
 });
