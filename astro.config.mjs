e131e9d7ba77c18684738624ac0458453b20867d
Ahmed Marzook <43218910+ahmed-marzook@users.noreply.github.com>
2025-10-03 22:01:33 +0100

    Scope relative base to build only

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,16 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [react()],
-  vite: {
-    base: './',
-  },
-  build: {
-    assetsPrefix: '.',
-  },
+export default defineConfig(({ command }) => {
+  const isBuild = command === 'build';
+
+  return {
+    integrations: [react()],
+    vite: {
+      ...(isBuild ? { base: './' } : {}),
+    },
+    build: {
+      ...(isBuild ? { assetsPrefix: '.' } : {}),
+    },
+  };
 });
