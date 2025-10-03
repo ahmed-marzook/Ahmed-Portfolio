ac6a1f5a259ab24f349339c9d27e4d4b3d72b6ac
Ahmed Marzook <m4marzook@gmail.com>
2025-10-03 23:28:14 +0100

    removed merge changes error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,12 @@
 // @ts-check
 import react from '@astrojs/react';
-<<<<<<< Updated upstream
-=======
 import sitemap from '@astrojs/sitemap';
->>>>>>> Stashed changes
+
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ahmed-marzook.github.io',
   base: '/Ahmed-Portfolio',
-<<<<<<< Updated upstream
-  integrations: [react()],
-=======
   integrations: [react(), sitemap()],
->>>>>>> Stashed changes
 });
