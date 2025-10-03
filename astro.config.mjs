0537bff128cd855148ba64de96f02d4b2fc21c1f
Ahmed Marzook <m4marzook@gmail.com>
2025-10-03 23:27:22 +0100

    feat: Add Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
 // @ts-check
 import react from '@astrojs/react';
+<<<<<<< Updated upstream
+=======
+import sitemap from '@astrojs/sitemap';
+>>>>>>> Stashed changes
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://ahmed-marzook.github.io',
   base: '/Ahmed-Portfolio',
+<<<<<<< Updated upstream
   integrations: [react()],
+=======
+  integrations: [react(), sitemap()],
+>>>>>>> Stashed changes
 });
